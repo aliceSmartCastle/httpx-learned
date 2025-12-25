38e7356534ca74884db65708ac9016be213df23c
aliceSmartCastle <shirahananami@gmail.com>
2025-12-25 08:46:13 -0200

    Add files via upload

--- QuickStart.py
+++ QuickStart.py
@@ -1,12 +1,13 @@
 import json.decoder
 import os
 from json import JSONDecodeError
+from pprint import pprint
 from typing import Literal, Any
 from enum import Enum, auto
 from io import BytesIO
 from PIL import Image, UnidentifiedImageError
 from PIL.ImageFile import ImageFile
-from httpx import Response, URL, get, post, put, head, delete, options, HTTPStatusError, codes
+from httpx import Response, URL, get, post, put, head, delete, options, HTTPStatusError, codes, stream, Cookies
 from UserAgentGet import User_Agent
 
 
@@ -17,7 +18,7 @@ class requestMethod(Enum):
     delete = auto()
     head = auto()
     options = auto()
-    status = auto
+    cookies = auto()
 
 
 def open_file(file_path: str = os.getcwd(), file_name: str = '', file_key: str = '') -> dict[str, bytes]:
@@ -39,8 +40,13 @@ def open_json(file_name: str, file_path: str = os.getcwd(), pretty: bool = True)
 def ResponseUrlState(url_link: str = '', method: str = 'get', wayData: dict = None, getParam: dict = None,
                      getterWay: Literal['params', 'params_url', 'normal_url'] = '', headers: dict = None,
                      postFile: Any = None, postJson: Any = None, post_content: bytes = '',
-                     extra_address: str = '') -> URL | Response:
+                     extra_address: str = '', add_cookies: dict | Cookies = None, Redirection: bool = False,
+                     set_timeout: float = 0.4,get_auth:Any=None) -> URL | Response:
     """
+     :param get_auth:    the auth argument of response get
+     :param set_timeout:   the timeout argument of response get
+     :param Redirection:  the Redirection argument of response get
+     :param add_cookies: the cookies argument of response get
      :param post_content: the content argument of response post
      :param extra_address: additional the address to the request get
      :param postJson:  the JSON argument of response post
@@ -66,32 +72,36 @@ def ResponseUrlState(url_link: str = '', method: str = 'get', wayData: dict = No
 
     # all test call this function
     # print(ResponseUrlState(method=requestMethod.requestEnum.name,getterWay='params')
-    response_param = {requestMethod.get.name: get(url_link + 'get', params=getParam, headers=headers),  #200 OK
-
-                      requestMethod.post.name: post(url_link + 'post', data=wayData, files=postFile, json=postJson,
-                                                    content=post_content),
-                      # 200 OK
-                      requestMethod.put.name: put(url_link + 'put', data=wayData),  # 200 OK
-                      requestMethod.delete.name: delete(url_link + 'delete'),  # 200 OK
-                      requestMethod.head.name: head(url_link + 'get'),  # 200 OK
-                      requestMethod.options.name: options(url_link + 'get')  #200 OK
-                      }
+    response_param = {
+        requestMethod.get.name: get(url_link + 'get', params=getParam, headers=headers, timeout=set_timeout),  #200 OK
+        requestMethod.cookies.name: get(url_link + 'cookies', cookies=add_cookies),
+        requestMethod.post.name: post(url_link + 'post', data=wayData, files=postFile, json=postJson,
+                                      content=post_content),
+        requestMethod.put.name: put(url_link + 'put', data=wayData),  # 200 OK
+        requestMethod.delete.name: delete(url_link + 'delete'),  # 200 OK
+        requestMethod.head.name: head(url_link + 'get'),  # 200 OK
+        requestMethod.options.name: options(url_link + 'get')  #200 OK
+        }
     # all test call this function
     # print(ResponseUrlState(method=requestMethod.requestEnum.name)
-    request_normal = {requestMethod.get.name: get(url_link + extra_address),  # 200 OK
-                      requestMethod.post.name: post(url_link),  #405 Method Not Allowed
-                      requestMethod.put.name: put(url_link),  #405 Method Not Allowed
-                      requestMethod.delete.name: delete(url_link),  # 405 Method Not Allowed
-                      requestMethod.head.name: head(url_link),  #  200 OK
-                      requestMethod.options.name: options(url_link)  #  200 OK
-                      }
+    request_normal = {
+        requestMethod.get.name: get(url_link + extra_address, follow_redirects=Redirection, timeout=set_timeout,auth=get_auth),
+        # 200 OK
+        requestMethod.post.name: post(url_link),  #405 Method Not Allowed
+        requestMethod.put.name: put(url_link),  #405 Method Not Allowed
+        requestMethod.delete.name: delete(url_link),  # 405 Method Not Allowed
+        requestMethod.head.name: head(url_link),  #  200 OK
+        requestMethod.options.name: options(url_link)  #  200 OK
+        }
 
     urlHttp = response_param.get(method)
+
     normalHttp = request_normal.get(method)
 
     match getterWay:
         case 'params':
             return urlHttp
+
         case 'params_url':
             param_url = urlHttp.url
             return param_url
@@ -116,49 +126,42 @@ def url_Content(url_link: str = '', needContent: Literal['text', 'change_encodin
                                                   getterWay="params", postFile=post_files, postJson=post_json,
                                                   post_content=postContent)
 
-
-
             post_type = information_Accept.headers.get('content-type')
-
-
 
             acceptType = ['application/json']
 
             if post_type in acceptType:
 
+                json_format_post = information_Accept.json()
 
-                    json_format_post = information_Accept.json()
+                parse_request = {'arg': json_format_post.get('arg'),
+                                 'data': json_format_post.get('data'),
+                                 'files': json_format_post.get('files'),
+                                 'form': json_format_post.get('form'),
+                                 'json': json_format_post.get('json'),
+                                 'origin': json_format_post.get('origin'),
+                                 'url': information_Accept.url,
+                                 'headers': information_Accept.headers,
 
-                    parse_request = {'arg': json_format_post.get('arg'),
-                                     'data': json_format_post.get('data'),
-                                     'files': json_format_post.get('files'),
-                                     'form': json_format_post.get('form'),
-                                     'json': json_format_post.get('json'),
-                                     'origin': json_format_post.get('origin'),
-                                     'url': information_Accept.url,
-                                     'headers': information_Accept.headers,
+                                 }
 
-                                     }
+                def valid_keyCheck(datas: dict = None):
+                    if originHidden:
+                        datas.pop('origin')
+                    if urlHidden:
+                        datas.pop('url')
+                    if headersHidden:
+                        datas.pop('headers')
+                    request_it = {}
+                    for i, v in datas.items():
+                        if (v == {}) or (v is None) or (v == ''):
+                            ...
+                        else:
+                            request_it.update({i: v})
+                    return request_it
 
-
-
-                    def valid_keyCheck(datas: dict = None):
-                        if originHidden:
-                            datas.pop('origin')
-                        if urlHidden:
-                            datas.pop('url')
-                        if headersHidden:
-                            datas.pop('headers')
-                        request_it = {}
-                        for i, v in datas.items():
-                            if (v == {}) or (v is None) or (v == ''):
-                                ...
-                            else:
-                                request_it.update({i: v})
-                        return request_it
-
-                    pretty_dict = valid_keyCheck(parse_request)
-                    return pretty_dict
+                pretty_dict = valid_keyCheck(parse_request)
+                return pretty_dict
 
 
             else:
@@ -215,14 +218,87 @@ def http_headers(url_link: str = '', content_type: bool = False):
         return headInfo.headers
 
 
+def stream_loop(url_link: str = '', stream_type: Literal['bytes', 'text', 'lines', 'raw', 'content-length'] = 'text',
+                setting_length: int = 1000) -> str | list[Any]:
+    stream_list = []
+
+    if url_link == '':
+        url_link = 'https://httpbin.org/'
+    upper_get = requestMethod.get.name.upper()
+    with stream(method=upper_get, url=url_link) as st:
+        match stream_type:
+            case 'bytes':
+                for data in st.iter_bytes():
+                    stream_list.append(data)
+                return stream_list
+            case 'text':
+                for text in st.iter_text():
+                    stream_list.append(text)
+                return stream_list
+            case 'lines':
+                for line in st.iter_lines():
+                    stream_list.append(line)
+                return stream_list
+            case 'raw':
+                for raw in st.iter_raw():
+                    stream_list.append(raw)
+                return stream_list
+            case 'content-length':
+                if int(st.headers.get('content-length')) < setting_length:
+                    stream_list.append(st.read())
+                    return stream_list
+                else:
+                    return "setting length is not enough"
+
+            case '' | _:
+                return "unknown stream type"
+
+
+def http_cookie(url_link: str = '', add_cookie: dict = None) -> dict:
+    if url_link == '':
+        url_link = 'https://httpbin.org/'
+    cookie_fail = None
+    try:
+        cookie_info = ResponseUrlState(url_link=url_link, method=requestMethod.cookies.name, add_cookies=add_cookie,
+                                       getterWay='params')
+        cookie_fail = cookie_info
+        cookie_success = cookie_info.json()
+
+    except (JSONDecodeError, UnicodeDecodeError):
+        return {cookie_fail.status_code: cookie_fail.text}
+
+    return cookie_success
+
+
+def history_watch(url_link: str = '', redirects: bool = True) -> dict:
+    url_history = ResponseUrlState(url_link=url_link, method=requestMethod.get.name, Redirection=redirects)
+    first_status = url_history.status_code
+    next_status = url_history.next_request
+    url_history = url_history.history
+    return {'status': first_status, 'next_status': next_status, 'history': url_history}
+def authentication_http(url_link: str = '', auth: tuple[str, str] = None) -> str:
+    if url_link == '':
+        url_link ='https://practicetestautomation.com/practice-test-login/'
+    login_information = ResponseUrlState(url_link=url_link, method=requestMethod.get.name, get_auth=auth,set_timeout=1)
+    if login_information.status_code == codes.OK:
+        return "login success"
+    else:
+        return "login fail"
+
+
 if __name__ == '__main__':
     my_UA = User_Agent()
+    this_cookies = Cookies()
     # print(ResponseUrlState(method=requestMethod.get.name,headers={"cookie":"best food"},
     # url_link='https://www.python.org/'))
     files_here = open_file(file_name='pyproject.toml', file_key='upload-files')
     json_test = open_json(file_name="hello.json", pretty=False)
-    print(http_headers(url_link='https://www.python.org/'))
+    #print(http_cookie(url_link='https://http.cat/',add_cookie={"American": "The USA", "English": "UK"}))
+    #print(history_watch(url_link='https://dummyjson.com/', redirects=True))
+    pprint(authentication_http(auth=(' student','Password1238')))
 
-    #print(url_Content(needContent='post', postContent=b'hello world!', headersHidden=False))
+    #print(http_headers(url_link='https://www.python.org/'))
+
+    #print(url_Content(needContent='post', postContent=hello world!', headersHidden=False))
 #print(binary_data(url_link='https://www.python.org/static/community_logos/python-logo.png'))
 #print(fetch_json(url_link='https://dummyjson.com/test'))
--- README.md
+++ README.md
@@ -29,12 +29,15 @@ class requestMethod(Enum):
     delete = auto()
     head = auto()
     options = auto()
+    cookies = auto()
  ```
 __enter__ the main function
 ``` python
 def ResponseUrlState(url_link: str = '', method: str = 'get', wayData: dict = None, getParam: dict = None,
                      getterWay: Literal['params', 'params_url', 'normal_url'] = '', headers: dict = None,
-                     postFile: Any = None, postJson: Any = None) -> URL | Response:
+                     postFile: Any = None, postJson: Any = None, post_content: bytes = '',
+                     extra_address: str = '', add_cookies: dict | Cookies = None, Redirection: bool = False,
+                     set_timeout: float = 0.4,get_auth:Any=None) -> URL | Response:
    ```     
 
 __check__ the method is valid httpx request  
@@ -54,8 +57,9 @@ __match__ different httpx passing  parameters  of functions
 ``` python
     # all test call this function   
     # print(ResponseUrlState(method=requestMethod.requestEnum.name,getterWay='params')
-    response_param = {requestMethod.get.name: get(url_link + 'get', params=getParam, headers=headers), #200 OK
+    response_param = {rrequestMethod.get.name: get(url_link + 'get', params=getParam, headers=headers,timeout=set_timeout),,  #200 OK
                       requestMethod.post.name: post(url_link + 'post', data=wayData, files=postFile, json=postJson), #200 OK
+                      requestMethod.cookies.name: get(url_link + 'cookies', cookies=add_cookies),
                       requestMethod.put.name: put(url_link + 'put', data=wayData), # 200 OK
                       requestMethod.delete.name: delete(url_link + 'delete'), # 200 OK
                       requestMethod.head.name: head(url_link + 'get'),       # 200 OK
@@ -67,7 +71,7 @@ __build__ the not parameters  of http response dictionary
 ``` python
     # all test call this function   
     # print(ResponseUrlState(method=requestMethod.requestEnum.name)
-    request_normal = {requestMethod.get.name: get(url_link),  # 200 OK
+    request_normal = {equestMethod.get.name: get(url_link + extra_address, follow_redirects=Redirection, timeout=set_timeout,auth=get_auth), 
                       requestMethod.post.name: post(url_link), #405 Method Not Allowed
                       requestMethod.put.name: put(url_link),    #405 Method Not Allowed
                       requestMethod.delete.name: delete(url_link), # 405 Method Not Allowed
@@ -400,6 +404,115 @@ __or__ content_type information
 __it's__ calling the function true method  
 _**__step 12 finish__**_
 ***
+## Step 13 :  Streaming Responses
+__construct__ the streaming response function
+``` python
+def stream_loop(url_link: str ='',stream_type: Literal['bytes','text','lines','raw','content-length']='text',setting_length:int=1000) -> str | list[Any]:
+    stream_list =[]
+
+    if url_link == '':
+        url_link = 'https://httpbin.org/'
+    upper_get=requestMethod.get.name.upper()
+    with stream(method=upper_get,url=url_link) as st:
+        match stream_type :
+            case 'bytes':
+             for data in st.iter_bytes():
+               stream_list.append(data)
+             return stream_list
+            case 'text':
+                for text in st.iter_text():
+                  stream_list.append(text)
+                return stream_list
+            case 'lines':
+                for line in st.iter_lines():
+                  stream_list.append(line)
+                return stream_list
+            case 'raw':
+                for raw in st.iter_raw():
+                 stream_list.append(raw)
+                return stream_list
+            case 'content-length':
+                if int(st.headers.get('content-length')) < setting_length:
+                    stream_list.append(st.read())
+                    return stream_list
+                else:
+                    return "setting length is not enough"
+
+            case ''|_:
+                return "unknown stream type"
+
+ ```
+__this__ function can do thing
+1. use the binary content for the streaming response       #stream_type = 'bytes'
+2. use the text content for the streaming response        #stream_type = 'text'
+3. use the lines content for the streaming response       #stream_type = 'lines'
+4. use the raw content for the streaming response        #stream_type = 'raw'
+5. use the content length for the streaming response     #stream_type = 'content-length'
+
+_**__step 13 finish__**_
+***
+## Step 14 : Cookies
+__Send__ to cookie to url is dangerous behavior
+__Don't__ using this function in usually time 
+``` python
+  def http_cookie(url_link: str = '', add_cookie: dict = None) ->  dict:
+
+        if url_link == '':
+         url_link = 'https://httpbin.org/'
+        cookie_fail= None
+        try:
+              cookie_info = ResponseUrlState(url_link=url_link, method=requestMethod.cookies.name, add_cookies=add_cookie,
+                                       getterWay='params')
+              cookie_fail = cookie_info
+              cookie_success = cookie_info.json()
+
+        except (JSONDecodeError,UnicodeDecodeError):
+            return {cookie_fail.status_code : cookie_fail.text}
+
+        return cookie_success
+```
+__error__ you can see the error info  
+__success__ you can see the send to cookie info
+_**__step 13 finish__**_
+***
+## Step 15 :  Redirection and History
+__construct__ the see the redirection and history function
+``` python
+def history_watch(url_link:str ='' , redirects:bool=True) ->dict:
+    url_history = ResponseUrlState(url_link=url_link,method=requestMethod.get.name,Redirection=redirects)
+    first_status =url_history.status_code
+    next_status = url_history.next_request
+    url_history = url_history.history
+    return {'status':first_status,'next_status':next_status,'history':url_history}
+ ```
+__this__ function can do thing
+1. see the url status code
+2. see the next url status code
+3. see the url history
+_**__step 15 finish__**_
+***
+## Step 16 :  Authentication
+__construct__ the simple authentication function
+``` python
+ def authentication_http(url_link: str = '', auth: tuple[str, str] = None) -> str:
+    if url_link == '':
+        url_link ='https://practicetestautomation.com/practice-test-login/'
+    login_information = ResponseUrlState(url_link=url_link, method=requestMethod.get.name, get_auth=auth,set_timeout=1)
+    if login_information.status_code == codes.OK:
+        return "login success"
+    else:
+        return "login fail"
+
+ ```
+__this__ function can do thing  
+test simple url authentication   
+_**__step 16 finish__**_
+***
+## [click to connect this md file reference document ](https://www.python-httpx.org/quickstart/)
+
+
+
+
 
 
 
